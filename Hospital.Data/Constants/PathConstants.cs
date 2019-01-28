459b3ce34b49c375f5ea8d28cf2333e5185d3e8e
Lana Shakhatreh <34197175+lanamshakhatreh@users.noreply.github.com>
2019-01-28 03:47:03 +0200

    Add files via upload

---
+++ Hospital.Data/Constants/PathConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace Hospital.Data.Constants
+{
+    public class PathConstants
+    {
+        public const string PathImage = "Content/images/";
+
+        public const string PathPDF = "../../Content/PDF/";
+    }
+}
